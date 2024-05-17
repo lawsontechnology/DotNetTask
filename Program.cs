3c458715c0ecddfde3bf52c5d238736a774e6c19
Lawal <141651904+lawsontechnology@users.noreply.github.com>
2024-05-17 11:53:29 +0100

    Implemented CRUD operations for questions

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+
+using ApplicationFormTask.Core.Application.Implementation.Service;
+using ApplicationFormTask.Core.Application.Interface.Repositories;
+using ApplicationFormTask.Core.Application.Interface.Services;
+using ApplicationFormTask.Infrastructure.Context;
+using ApplicationFormTask.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +16,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApplicationForm", Version = "v1" });
+});
+
+builder.Services.AddDbContext<ApplicationFormTaskContext>(opt => opt.UseMySQL(builder.Configuration.GetConnectionString("ApplicationForm")));
+builder.Services.AddScoped<IAuditLogRepository, AuditLogRepository>();
+builder.Services.AddScoped<IChoiceRepository,  ChoiceRepository>();
+builder.Services.AddScoped<ICustomQuestionRepository, CustomQuestionRepository>();
+builder.Services.AddScoped<IDateQuestionRepository, DateQuestionRepository>();
+builder.Services.AddScoped<IDropdownQuestionRepository, DropdownQuestionRepository>();
+builder.Services.AddScoped<IMultipleQuestionRepository, MultipleQuestionRepository>();
+builder.Services.AddScoped<INumericQuestionRepository,  NumericQuestionRepository>();
+builder.Services.AddScoped<IParagraphQuestionRepository,  ParagraphQuestionRepository>();
+builder.Services.AddScoped<IPersonalInformationRepository,  PersonalInformationRepository>();
+builder.Services.AddScoped<IProgramEntityRepository, ProgramEntityRepository>();
+builder.Services.AddScoped<IYesOrNoQuestionRepository, YesOrNoQuestionRepository>();
+builder.Services.AddScoped<IAuditLogService, AuditLogService>();
+builder.Services.AddScoped<ICustomQuestionService, CustomQuestionService>();
+builder.Services.AddScoped<IPersonalInformationService,  PersonalInformationService>();
+builder.Services.AddScoped<IProgramEntityService,  ProgramEntityService>();
+builder.Services.AddScoped<IQuestionTypesService, QuestionTypesService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +52,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
