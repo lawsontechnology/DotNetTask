343867297c6cd0506ee02a82f4f4839ca97e9c1f
Lawal <141651904+lawsontechnology@users.noreply.github.com>
2024-05-17 11:52:14 +0100

    Implemented CRUD operations for questions

---
+++ Migrations/20240517100008_ApplicationFormTask.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationFormTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationFormTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Action = table.Column<string>(type: "longtext", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "customQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionText = table.Column<string>(type: "longtext", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    OtherOption = table.Column<string>(type: "longtext", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "dateQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dateQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "dropdownQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    OtherOption = table.Column<string>(type: "longtext", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_dropdownQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "multipleQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    OtherOption = table.Column<string>(type: "longtext", nullable: true),
+                    MaxChoiceCount = table.Column<byte>(type: "tinyint unsigned", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_multipleQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "numericQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_numericQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "paragraphQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paragraphQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PersonalInformation",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    FirstName = table.Column<string>(type: "longtext", nullable: false),
+                    LastName = table.Column<string>(type: "longtext", nullable: false),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "longtext", nullable: false),
+                    Nationality = table.Column<string>(type: "longtext", nullable: false),
+                    CurrentResidence = table.Column<string>(type: "longtext", nullable: false),
+                    IDNumber = table.Column<string>(type: "longtext", nullable: false),
+                    DOB = table.Column<DateOnly>(type: "date", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    QuestionTypeToAnswer = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalInformation", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "programs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Title = table.Column<string>(type: "longtext", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_programs", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "yesOrNoQuestions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    QuestionType = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_yesOrNoQuestions", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "choices",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: true),
+                    DropdownQuestionId = table.Column<string>(type: "varchar(255)", nullable: true),
+                    MultipleQuestionId = table.Column<string>(type: "varchar(255)", nullable: true),
+                    CustomQuestionId = table.Column<string>(type: "varchar(255)", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    DeletedBy = table.Column<string>(type: "longtext", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "longtext", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_choices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_choices_customQuestions_CustomQuestionId",
+                        column: x => x.CustomQuestionId,
+                        principalTable: "customQuestions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_choices_dropdownQuestions_DropdownQuestionId",
+                        column: x => x.DropdownQuestionId,
+                        principalTable: "dropdownQuestions",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_choices_multipleQuestions_MultipleQuestionId",
+                        column: x => x.MultipleQuestionId,
+                        principalTable: "multipleQuestions",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_choices_CustomQuestionId",
+                table: "choices",
+                column: "CustomQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_choices_DropdownQuestionId",
+                table: "choices",
+                column: "DropdownQuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_choices_MultipleQuestionId",
+                table: "choices",
+                column: "MultipleQuestionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+
+            migrationBuilder.DropTable(
+                name: "choices");
+
+            migrationBuilder.DropTable(
+                name: "dateQuestions");
+
+            migrationBuilder.DropTable(
+                name: "numericQuestions");
+
+            migrationBuilder.DropTable(
+                name: "paragraphQuestions");
+
+            migrationBuilder.DropTable(
+                name: "PersonalInformation");
+
+            migrationBuilder.DropTable(
+                name: "programs");
+
+            migrationBuilder.DropTable(
+                name: "yesOrNoQuestions");
+
+            migrationBuilder.DropTable(
+                name: "customQuestions");
+
+            migrationBuilder.DropTable(
+                name: "dropdownQuestions");
+
+            migrationBuilder.DropTable(
+                name: "multipleQuestions");
+        }
+    }
+}
